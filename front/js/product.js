b4d6fede76a1026da2efc8c803384d5cc12adc50
Nassim Bourebia <b.nassim01@gmail.com>
2022-12-26 23:25:12 +0100

    add function for products page

--- front/js/product.js
+++ front/js/product.js
@@ -4,4 +4,69 @@ const id = urlParamss.get("id")
 
 fetch(`http://localhost:3000/api/products/${id}`) // Utiliser String interpolation avec `` 
 .then(response => response.json())
-.then(response => console.log(response));
+.then(response => displayData(response));
+
+
+function displayData (kanap) {
+
+                                             //Ce code peux être écrit d'une maniére plus compact 
+    const name = kanap.name;                //---> const {_id, name, imageUrl, description, colors, price, altTxt} = kanap ; 
+    const imageUrl = kanap.imageUrl; 
+    const description = kanap.description;
+    const colors = kanap.colors;
+    const price = kanap.price;
+    const altTxt = kanap.altTxt; 
+
+    makeImage(altTxt, imageUrl); 
+    makeTitle(name);
+    makePrice(price); 
+    makeDescription(description);
+    makeColors(colors);
+   
+
+}
+    function makeImage(altTxt, imageUrl) {
+    
+    const image = document.createElement("img")
+    image.src = imageUrl;
+    image.alt = altTxt;
+    const itemsImg = document.querySelector(".item__img").appendChild(image);
+   
+    }
+
+    function makeTitle (name) {
+       
+        const h1 = document.querySelector("#title");
+        h1.textContent = name ; 
+    }
+
+    function makePrice(price) {
+
+        const span = document.querySelector("#price");
+        span.textContent = price;
+    }
+
+    function makeDescription (description) {
+
+        const paragraphDescrip = document.querySelector("#description"); 
+        paragraphDescrip.textContent = description; 
+    }
+
+    function makeColors(colors) { 
+
+        const select = document.querySelector("#colors") 
+
+        if (select != null) {
+            colors.forEach((color) => {
+                const option = document.createElement("option");
+                option.value = color; 
+                option.textContent = color;
+                select.appendChild(option)
+                
+            });
+        }
+     }
+
+
+
+  
